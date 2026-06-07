f49da9e0bb34e997d79d9cc6332c3fd373802c0e
monster1389 <3374155431@qq.com>
2026-06-07 22:27:11 +0800

    chore: add vitest, supertest, expand config.js

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+const path = require('path');
+
 module.exports = {
   port: parseInt(process.env.PORT, 10) || 8090,
+  assetsDir: process.env.ASSETS_DIR || path.join(__dirname, 'assets'),
+  chromiumFallback: process.env.CHROMIUM_PATH || null,
 };
